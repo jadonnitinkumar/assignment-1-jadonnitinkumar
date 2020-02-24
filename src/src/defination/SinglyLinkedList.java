14b9185402812ac2b7605117a1527009ba5fa86c
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-02-24 17:52:45 +0530

    create private E data field and Private Node<E> next

--- src/src/defination/SinglyLinkedList.java
+++ src/src/defination/SinglyLinkedList.java
@@ -28,6 +28,8 @@ public class SinglyLinkedList<E>implements SinglyADT<E>{
 
     }
     private static class Node<E> implements Comparable<E>{
+        private E data;
+        private Node<E> next;
 
         @Override
         public int compareTo(E o) {
