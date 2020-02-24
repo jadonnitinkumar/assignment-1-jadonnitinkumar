883a1691911ddb4ca3481b2dcaddb9c9ac796e28
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-02-24 19:12:36 +0530

    This list contains the phone numbers.

--- src/src/person/Person.java
+++ src/src/person/Person.java
@@ -1,6 +1,17 @@
 package person;
 
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person implements Comparable<Person>{
+    private String fName;
+    private String lName;
+    private String email;
+    private long phoneNumber;
+
+    List<Long> list = new ArrayList<>();
+
 
     @Override
     public int compareTo(Person o) {
