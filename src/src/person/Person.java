f1b778ff340a9b4527925f173b163fc47d0d8ecf
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-02-24 19:15:54 +0530

    Create constructor

--- src/src/person/Person.java
+++ src/src/person/Person.java
@@ -12,6 +12,17 @@ public class Person implements Comparable<Person>{
 
     List<Long> list = new ArrayList<>();
 
+    public Person(){
+
+    }
+
+    public  Person(String fName, String lName , String email, long phoneNumber, List<Long> list) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.list = list;
+    }
 
     @Override
     public int compareTo(Person o) {
