d20a8a72cac1ecb1b12d6da2c0cce42033d97607
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-02-24 17:46:47 +0530

    create SinglyLinkedList class implements SinglyADT and override All Methods

--- src/src/defination/SinglyLinkedList.java
+++ src/src/defination/SinglyLinkedList.java
@@ -1,4 +1,30 @@
 package defination;
 
-public class SinglyLinkedList {
+import adt.SinglyADT;
+
+public class SinglyLinkedList<E>implements SinglyADT<E>{
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
